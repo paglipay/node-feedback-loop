cbaeeb2b42d58078d5f5c66a0cc1b005c0b7e610
Paul Aglipay <paglipay@gmail.com>
2022-07-11 15:08:46 -0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hi')
+const { exec } = require("child_process");
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
