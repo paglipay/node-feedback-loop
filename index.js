210719325c076f736d1fe8f1822c37dbc97cb6d1
Paul <paglipay@gmail.com>
2022-07-11 17:31:56 -0700

    'pulling'

--- index.js
+++ index.js
@@ -1,39 +1,6 @@
 console.log('hi')
 const { exec } = require("child_process");
 
-// exec("ls -la", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
-// exec("git config --global user.email paglipay@gmail.com && git config --global user.name 'Paul Aglipay' && git add . && git commit -m 'pulling' && git pull origin main && echo hostname >> notes.md && git add . && git commit -m 'hi' && git push origin main", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
-// exec("echo hostname >> notes.md && cd ../../test && git clone https://github.com/paglipay/node-feedback-loop.git && cd ./node-feedback-loop && npm install && npm start && code .", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
 const myExec = (cmd) => {
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
@@ -53,6 +20,6 @@ const myCmds = (cmds) => {
 
 
 const cmds = ["git config --global user.email paglipay@gmail.com && git config --global user.name 'Paul Aglipay' && git add . && git commit -m 'pulling' && git pull origin main && echo hostname >> notes.md && git add . && git commit -m 'hi' && git push origin main",
-    "echo hostname >> notes.md && cd ../../test && git clone https://github.com/paglipay/node-feedback-loop.git && cd ./node-feedback-loop && npm install && npm start && code ."]
+    "echo hostname >> notes.md && cd ../../test2 && git clone https://github.com/paglipay/node-feedback-loop.git && cd ./node-feedback-loop && npm install && npm start && code ."]
 
 myCmds(cmds);
--- notes.md
+++ notes.md
@@ -35,3 +35,4 @@ hostname
 hostname  
 hostname  
 hostname  
+hostname  
